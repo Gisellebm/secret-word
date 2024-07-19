87ee15325a79eb21a895bca8b341ef6469a8b4d5
Gisellebm <gisellebrasil.cf@gmail.com>
2024-07-19 11:23:36 -0300

    Finalizado página inicial

---
+++ src/data/word.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Macã", "Abacaxi", "Laranja", "Manga"],
+    corpo: ["Braço", "Perna", "Pescoço", "Cintura", "Olhos"],
+    computador: ["Processador", "Placa-maça", "Memória", "Fonte", "Gabinete"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    programacao: ["Linguagem", "Framework", "IDE", "Biblioteca", "Framework"],
+}
